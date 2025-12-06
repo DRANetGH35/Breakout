4b17bb8288ce085c99cf3463216a66fc614a9f3d
Grok <devanaptaker@gmail.com>
2025-12-06 12:12:09 -0800

    add: brick colors

--- Brick.py
+++ Brick.py
@@ -2,12 +2,12 @@ import pygame
 
 
 class Brick(pygame.sprite.Sprite):
-    def __init__(self, screen, pos):
+    def __init__(self, screen, pos, color):
         pygame.sprite.Sprite.__init__(self)
         self.screen = screen
         self.pos = pygame.Vector2(pos)
         self.width = 100
         self.height = 50
-        self.color = (255, 255, 255)
+        self.color = color
     def update(self, screen):
         pygame.draw.rect(screen, self.color, (self.pos[0], self.pos[1], self.width, self.height))
--- Wall.py
+++ Wall.py
@@ -6,8 +6,11 @@ screen = pygame.display.set_mode((1280, 720))
 
 class Wall(pygame.sprite.Group):
     def __init__(self):
+        self.color = (255, 0, 0)
         pygame.sprite.Group.__init__(self)
         for x in range(10):
+            self.color = (255, 0, 0)
             for y in range(3):
-                new_brick = Brick(screen, (x * 105 + 100, y * 55 + 10))
+                self.color = (255, 0, self.color[2] + 60)
+                new_brick = Brick(screen, (x * 105 + 100, y * 55 + 10), self.color)
                 self.add(new_brick)
